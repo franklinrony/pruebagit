82760b7ca45b239546f32f5fc62e6abda175904b
franklinrony <franklinrony2@gmail.com>
2015-06-14 16:28:42 +0200

    Agregado hola mundo

--- src/ejemplo/git/EjemploGit.java
+++ src/ejemplo/git/EjemploGit.java
@@ -16,6 +16,7 @@ public class EjemploGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
